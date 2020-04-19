6d73c06b8ed0a5085bd908f9aa2dab16a79358cf
glebsonik <leeroyglebchik@gmail.com>
2020-04-19 13:20:49 +0300

    Initial commit

---
+++ graphics_visual.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import random
+
+# fig, ax = plt.subplots()
+# x1 = np.linspace(1,10,10)
+x = np.arange(1, 5)
+y1 = np.exp(x)
+print(y1)
+plt.plot(x, y1)
+plt.show()
