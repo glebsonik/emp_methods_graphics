43038d16c5028df99367c12ce427bb4cd2129d7c
glebsonik <leeroyglebchik@gmail.com>
2020-04-20 11:45:15 +0300

    Added PrettyTable

--- graphics_visual.py
+++ graphics_visual.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import random
+from prettytable import PrettyTable
 
 # [start, end)
 def elementInRnage(elements, start, end):
@@ -19,15 +20,14 @@ def makeIntervals(samples, start, delt, count):
     for i in range(1, count+1):
         currentStart = start+(i-1)*delt
         currentEnd = start + i*delt
-        print(currentStart, currentEnd)
-        intervals.append([(currentStart + currentEnd)/2, elementInRnage(samples, currentStart, currentEnd)])
+        intervals.append([(currentStart + currentEnd)/2,
+                          elementInRnage(samples, currentStart, currentEnd),
+                          (currentStart, currentEnd)])
     intervals[-1][1] += 1
     return intervals
 
-mas = [1,2,3,4,5,6,7,8,9,10]
-print(makeIntervals(mas, 1, 9/3, 3))
-lam = 9
 scale = 100
+lam = 9
 np.random.seed(25)
 expX = np.random.exponential(lam, scale)
 
@@ -36,7 +36,7 @@ sigma = 0.25
 normX = np.random.normal(mu, sigma, scale)
 normX.sort()
 expX.sort()
-print("norm: ", normX, "\n exp: ", expX)
+# print("norm: ", normX, "\n exp: ", expX)
 
 d = normX[-1] - normX[0]
 print("d= ", d, " (Размах)")
@@ -44,7 +44,31 @@ intervalCount = 5
 
 delta = d/intervalCount
 print("Delta = ", delta)
-print(makeIntervals(normX, normX[0], delta, intervalCount))
+normIntervals = makeIntervals(normX, normX[0], delta, intervalCount)
+intTuples = []
+intElCount = []
+intAver = []
+for interval in normIntervals:
+    intAver.append(interval[0])
+    intElCount.append(interval[1])
+    intTuples.append(interval[2])
+
+nIntTable = PrettyTable()
+nIntTable.add_row(intTuples)
+nIntTable.add_row(intElCount)
+nIntTable.add_row(intAver)
+print(nIntTable)
+
+localEstTableNorm = PrettyTable(["Ji", "xi*", "ni", "ui", "ni*ui", "ni*ui^2", "ni(ui + 1)^2"])
+falseZero = normX[49]
+for interval in normIntervals:
+    ui = (interval[0] - falseZero)/delta
+    niui = interval[1] * ui
+    niuisqr = interval[1] * ui * ui
+    controlCl = interval[1]*pow((ui + 1), 2)
+    currentRow = [interval[2], interval[0], interval[1], ui, niui, niuisqr, controlCl]
+    localEstTableNorm.add_row(currentRow)
+print(localEstTableNorm)
 
 #Graphic section
 plt.hist(normX, 50)
